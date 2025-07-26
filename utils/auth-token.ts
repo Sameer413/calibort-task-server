5de0fec20f912c5a7cef44ec72aed642fe64dbf3
Sameer413 <92072016+Sameer413@users.noreply.github.com>
2025-07-26 19:13:22 +0530

    deployment issue

--- utils/auth-token.ts
+++ utils/auth-token.ts
@@ -1,11 +1,12 @@
+require("dotenv").config();
 import jwt from "jsonwebtoken";
 
 const generateToken = (payload: object): string => {
-    return jwt.sign(payload, "SecretKey", { expiresIn: '5h' })
+    return jwt.sign(payload, process.env.SECRET_KEY!, { expiresIn: '5h' })
 }
 
 export const verifyToken = (token: string) => {
-    return jwt.verify(token, "SecretKey");
+    return jwt.verify(token, process.env.SECRET_KEY!);
 };
 
 export default generateToken;
